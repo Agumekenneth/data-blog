2c8555fbeadd9246b4090fd22b8ae42c49eb5cc2
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-02-19 11:16:45 +0300

    intial change

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+export default function Home(){
+  let firstNumber = 938
+  let secondNumner = 7
+  let sum = firstNumber + secondNumner
+
+return (
+<div>
+  <h1>DATA SCIENCE FOR AFRICA </h1>
+  <h2>An intiative for 2025</h2>
+  <p>Welcome to our website</p>
+  <strong>{10+10*1000}</strong> 
+  <br/>
+  <em>{sum}</em>
+
+  <p>Greeting,{'username'}</p>
+</div>
+)
+}
