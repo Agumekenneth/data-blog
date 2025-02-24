3774095db48c1ef93e988491bb872eb08b27c8fa
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-02-24 11:50:30 +0300

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,10 @@ export default function Home(){
   let firstNumber = 938
   let secondNumner = 7
   let sum = firstNumber + secondNumner
-
+  let username = 'John Doe'
+  let username2 = 'jane'
 return (
-<div>
+<div style={{color: "red",backgroundColor: "#bec1f7", border: "10px solid black",padding:"10px",margin:"50px"}}>
   <h1>DATA SCIENCE FOR AFRICA </h1>
   <h2>An intiative for 2025</h2>
   <p>Welcome to our website</p>
@@ -12,7 +13,8 @@ return (
   <br/>
   <em>{sum}</em>
 
-  <p>Greeting,{'username'}</p>
+  <p>Greetings,{username}🫡</p>
+  <p>Greetings,{username2}🫡</p>
 </div>
 )
 }
