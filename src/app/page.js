1112822876a1dfb333d0ab7b32854633c859240d
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-02-24 11:55:45 +0300

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,16 @@ export default function Home(){
   let username = 'John Doe'
   let username2 = 'jane'
 return (
-<div style={{color: "#0f0b14",backgroundColor: "#bec1f7", border: "10px solid black",padding:"10px",margin:"50px",borderRadius:"30px"}}>
-  <h1>DATA SCIENCE FOR AFRICA </h1>
-  <h2>An intiative for 2025</h2>
-  <p>Welcome to our website</p>
-  <strong>{10+10*1000}</strong> 
-  <br/>
-  <em>{sum}</em>
+  <div style={{color: "#0f0b14", backgroundColor: "#bec1f7", border: "10px solid black", padding: "10px", margin: "50px", borderRadius: "30px"}}>
+    <h1>DATA SCIENCE</h1>
+    <h2>A very nice initiative for 2025</h2>
+    <p>Data science is the field of study that combines domain expertise, programming skills, and statistical knowledge to extract meaningful insights from data.</p>
+    <strong>{10 + 10 * 1000}</strong>
+    <br/>
+    <em>{sum}</em>
 
-  <p>Greetings,{username}🫡</p>
-  <p>Greetings,{username2}🫡</p>
-</div>
+    <p>Greetings, {username}🫡</p>
+    <p>Greetings, {username2}🫡</p>
+  </div>
 )
 }
