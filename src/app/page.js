39f50930af1321d22a8916a85790fda6f08473fc
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-02-24 15:41:40 +0300

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,46 @@
 import ChapterCard from "./Components/ChapterCard";
 import TitleCard from "./Components/TitleCard";
 
-export default function Home(){
-  let asscotiationChapters = [
+export default function Home() {
+
+  let associationChapters = [
     {
-      title: "DATA SCIENCE",
-      subtitle: "Data the new oil",
-      content: "Data science is the field of study that combines domain expertise, programming skills, and statistical knowledge to extract meaningful insights from data."
+      title: "Data Science",
+      subtitle: "Data the new oil.",
+      content: "Join our Data Science Club to explore the fascinating world of data analysis, machine learning, and artificial intelligence. Whether you're a beginner or an expert, our club offers resources, workshops, and a community to help you grow your skills and knowledge."
     },
+
     {
-      title: "CYBER SECURITY",
-      subtitle: "Guardians of the bytes",
-      content: "Cybersecurity is the practice of protecting systems, networks, and programs from digital attacks. These cyberattacks are usually aimed at accessing, changing, or destroying sensitive information; extorting money from users; or interrupting normal business processes."
+      title: "Cyber Security",
+      subtitle: "Guardians of the Byte.",
+      content: "Join our Cyber Security Club to delve into the critical field of protecting digital information. Our club provides insights into the latest security practices, ethical hacking, and defense strategies. Whether you're new to the field or a seasoned professional, we offer workshops, resources, and a community to enhance your skills and knowledge."
     },
     {
       title: "ROBOTICS",
-      subtitle: "We make nice bots",
-      content: "Robotics is the interdisciplinary field that combines computer science and engineering to design, build, and program robots. It involves the study of mechanical engineering, electrical engineering, and computer science to create intelligent machines that can perform tasks autonomously or with human guidance."
+      subtitle: "We make nice bots.",
+      content: "Join our Robotics Club to explore the exciting world of robotics and automation. Our club offers hands-on projects, workshops, and resources to help you build and program robots. Whether you're a beginner or an experienced roboticist, you'll find a community of like-minded enthusiasts to collaborate and grow with."
     }
-  ];
-  let ourStyles ={color: "#0f0b14",
-    
-      border: "10px solid black",
-       padding: "10px",
-        margin: "50px",
-         borderRadius: "30px"
+  ]
 
-  }
-return (
-  <div style={ourStyles}>
-    {asscotiationChapters.map(function(value, index) {
-      return (
-        <ChapterCard
-          key={index}
-          title={value.title}
-          subtitle={value.subtitle}
-          content={value.content}
-        />
-      );
-    })}
-  </div>
-)
+  return (
+    <div style={{ margin: "50px" }}>
+      <TitleCard
+        associationName={"CSE ASSOCIATION"}
+        departmentName={"Department of Computing and Technology"}
+      />
+      <TitleCard
+        associationName={"LAW SOCIETY"}
+        departmentName={"Department of Law"}
+      />
+      {associationChapters.map(function (chapter, index) {
+        return (
+          <ChapterCard
+            key={index}
+            title={chapter.title}
+            subtitle={chapter.subtitle}
+            content={chapter.content}
+          />)
+      })}
+    </div>
+  )
 }
